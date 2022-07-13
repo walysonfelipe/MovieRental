71ab03fa758d72fba7bd040cc1d9a1487a2090bc
walysonfelipe <walysonfelipe25@gmail.com>
2022-07-13 00:33:11 -0300

    Criando o aluguel do filme

--- src/modules/movies/useCases/createMovieRent/CreateMovieRentUseCase.ts
+++ src/modules/movies/useCases/createMovieRent/CreateMovieRentUseCase.ts
@@ -1,7 +1,45 @@
+import { AppError } from "../../../../errors/AppError";
+import { prisma } from "../../../../prisma/client";
 import { CreateMovieRentDTO } from "../../dtos/CreateMovieRentDTO";
 
 export class CreateMovieRentUseCase {
-    async execute({ movieId, userId}: CreateMovieRentDTO) {
-        
+    async execute({ movieId, userId}: CreateMovieRentDTO): Promise<void> {
+        const movieExist = await prisma.movie.findUnique({
+            where: {
+                id: movieId
+            }
+        });
+
+        if(!movieExist) {
+            throw new AppError("Movie does not exists!") 
+        }
+
+        const movieAlreadyRented = await prisma.movieRent.findFirst({
+         where: {
+            movieId
+         }
+        });
+
+        if(movieAlreadyRented){
+            throw new AppError("Movie already rented!")
+        }
+
+        const userExists = await prisma.user.findUnique({
+            where:{
+                id: userId
+            }
+        });
+
+        if(!userExists){
+            throw new AppError("User does not exists!");
+        }
+
+        await prisma.movieRent.create({
+            data: {
+                movieId,
+                userId
+            }
+        })
+
     }
 }
