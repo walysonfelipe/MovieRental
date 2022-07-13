1d49800e85bc16c616e2250b9c39149ce169bbf3
walysonfelipe <walysonfelipe25@gmail.com>
2022-07-13 00:49:35 -0300

    :rocket: Criando rota mostrando filmes com ordenação

--- src/routes/movie.routes.ts
+++ src/routes/movie.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { CreateMovieController } from "../modules/movies/useCases/createMovie/CreateMovieController";
 import { CreateMovieRentController } from "../modules/movies/useCases/createMovieRent/CreateMovieRentController";
+import { GetMoviesByReleaseDateController } from "../modules/movies/useCases/getMoviesByReleaseDate/GetMoviesByReleaseDateController";
 
 const createMovieController = new CreateMovieController();
 const createMovieRentController = new CreateMovieRentController();
-
+const getMoviesByReleaseDateController = new GetMoviesByReleaseDateController();
 const movieRoutes = Router();
 
 movieRoutes.post("/", createMovieController.handle);
+movieRoutes.get("/release", getMoviesByReleaseDateController.handle)
 movieRoutes.post("/rent", createMovieRentController.handle);
 export { movieRoutes };
